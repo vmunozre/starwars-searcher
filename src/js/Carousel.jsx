6b511186be3eeaa36c6e0ee7725adb0771d21a91
Victor Reiner <v.munozre@alumnos.urjc.es>
2017-09-15 19:31:51 +0200

    First version done

--- src/js/Carousel.jsx
+++ src/js/Carousel.jsx
@@ -9,7 +9,9 @@ class Carousel extends Component {
         this.state = { 
             index_item: 0,
             items_backup: [],
-            arrow_locked: false
+            arrow_locked: false,
+            next_page: '',
+            prev_page: ''
         }
         this.onClickArrow = this.onClickArrow.bind(this);
     }
@@ -23,6 +25,20 @@ class Carousel extends Component {
             .then((_people) => {
                 if(_people != undefined){
                     this.setState({ items_backup: _people.results});
+
+                    /* Save next and prev page if exist */
+                    if(_people.next != undefined){
+                        this.setState({next_page:_people.next});
+                    } else {
+                        this.setState({next_page:''});
+                    }
+
+                    if(_people.previous != undefined){
+                        this.setState({prev_page:_people.previous});
+                    } else {
+                        this.setState({prev_page:''});
+                    }
+
                 }                
             });
     }
@@ -41,32 +57,29 @@ class Carousel extends Component {
     renderItems(){
         if(this.state.items_backup != undefined && this.state.items_backup.length > 0){             
             var left_index = this.state.index_item - 1,
-                right_index = this.state.index_item + 1;            
-            if(left_index < 0){
-                left_index = (this.state.items_backup.length - 1);
-            }
-            if(right_index > (this.state.items_backup.length - 1)){
-                right_index = 0;
-            }
-            return (
-                <div className="Carousel-container">
-                    <div class="Carousel-arrow-container">
-                        <img id="left-arrow" className="Carousel-arrow" src={left_arrow} title="Left arrow" data-next={left_index} onClick={this.onClickArrow}/>
-                    </div>
-                   
-                    <CarouselItem 
-                        name={this.state.items_backup[this.state.index_item].name} 
-                        height={this.state.items_backup[this.state.index_item].height} 
-                        mass={this.state.items_backup[this.state.index_item].mass} 
-                        gender={this.state.items_backup[this.state.index_item].gender}
-                        birth_year={this.state.items_backup[this.state.index_item].birth_year}
-                    />
-                    <div class="Carousel-arrow-container">
-                        <img id="right-arrow" className="Carousel-arrow" src={right_arrow} title="Right arrow" data-next={right_index} onClick={this.onClickArrow}/>
-                    </div>
-                </div>
-            );
+                right_index = this.state.index_item + 1,      
+                item = this.state.items_backup[this.state.index_item];
+            if(item != undefined){
             
+                return (
+                    <div className="Carousel-container">
+                        <div class="Carousel-arrow-container">
+                            <img id="left-arrow" className="Carousel-arrow" src={left_arrow} title="Left arrow" data-next={left_index} onClick={this.onClickArrow}/>
+                        </div>
+                    
+                        <CarouselItem 
+                            name={item.name} 
+                            height={item.height} 
+                            mass={item.mass} 
+                            gender={item.gender}
+                            birth_year={item.birth_year}
+                        />
+                        <div class="Carousel-arrow-container">
+                            <img id="right-arrow" className="Carousel-arrow" src={right_arrow} title="Right arrow" data-next={right_index} onClick={this.onClickArrow}/>
+                        </div>
+                    </div>
+                );
+            }
         } else {
             return (
                 <div className="Carousel-container">
@@ -86,12 +99,12 @@ class Carousel extends Component {
                 right_arrow = document.getElementById('right-arrow'),
                 left_index = data_next - 1,
                 right_index = data_next + 1;
-                    
-            if(left_index < 0){
-                left_index = this.state.items_backup.length - 1;
+            
+            if(left_index < -1){
+                this.loadPage(this.state.prev_page);
             }
-            if(right_index > (this.state.items_backup.length - 1)){
-                right_index = 0;
+            if(right_index > (this.state.items_backup.length)){
+                this.loadPage(this.state.next_page);
             }
             left_arrow.setAttribute('data-next',left_index);
             right_arrow.setAttribute('data-next',right_index);
@@ -99,8 +112,40 @@ class Carousel extends Component {
             this.setState({index_item: data_next}, function(){
                 this.setState({arrow_locked: false});
             });
+        }        
+    }
+
+    loadPage(_page){
+        /* Load character page */
+        this.setState({ items_backup: []});
+        if(_page == ''){
+            _page = 'https://swapi.co/api/people?page=1';
         }
-        
+        fetch(_page)
+            .then((_response) => {
+                return _response.json()
+            })
+            .then((_people) => {
+                if(_people != undefined){
+                    this.setState({ items_backup: _people.results});
+
+                    /* Save next and prev page if exist */
+                    if(_people.next != undefined){
+                        this.setState({next_page:_people.next});
+                    } else {
+                        this.setState({next_page:''});
+                    }
+
+                    if(_people.previous != undefined){
+                        this.setState({prev_page:_people.previous});
+                    } else {
+                        this.setState({prev_page:''});
+                    }
+                }
+                this.setState({index_item: 0}, function(){                                     
+                    this.setState({arrow_locked: false});
+                });               
+            });
     }
 
 }
