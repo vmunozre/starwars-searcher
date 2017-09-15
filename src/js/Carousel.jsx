cf0a72133bd114d3e5d8eea6af4e1e5e696c3783
Victor Reiner <v.munozre@alumnos.urjc.es>
2017-09-15 17:03:26 +0200

    First carousel version

--- src/js/Carousel.jsx
+++ src/js/Carousel.jsx
@@ -1,20 +1,93 @@
 import React, { Component } from 'react';
+import left_arrow from '../assets/images/left-arrow.svg';
+import right_arrow from '../assets/images/right-arrow.svg';
 import '../css/Carousel.css';
 import CarouselItem from './CarouselItem.jsx';
 class Carousel extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            index_item: 0,
+            items_backup: []
+        }
+        this.onClickArrow = this.onClickArrow.bind(this);
+    }
+
+    componentWillMount() {
+        fetch('https://swapi.co/api/people/')
+            .then((_response) => {
+                return _response.json()
+            })
+            .then((_people) => {
+                if(_people != undefined){
+                    this.setState({ items_backup: _people.results});
+                }                
+            });
+    }
+
     render() {
         return (
             <div className="Carousel">
                 <h2 className="Carousel-title">Characters</h2>                
                 <div className="Carousel-container">
-                    <span>left</span>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <span>right</span>
+                    {this.renderItems()}
                 </div>
             </div>    
         );
     }
+
+    renderItems(){
+        if(this.state.items_backup != undefined && this.state.items_backup.length > 0){             
+            var left_index = this.state.index_item - 1,
+                right_index = this.state.index_item + 1;            
+            if(left_index < 0){
+                left_index = (this.state.items_backup.length - 1);
+            }
+            if(right_index > (this.state.items_backup.length - 1)){
+                right_index = 0;
+            }
+            return (
+                <div className="Carousel-container">
+                    <img id="left-arrow" className="Carousel-arrow" src={left_arrow} title="Left arrow" data-next={left_index} onClick={this.onClickArrow}/>
+                    <CarouselItem 
+                        name={this.state.items_backup[this.state.index_item].name} 
+                        height={this.state.items_backup[this.state.index_item].height} 
+                        mass={this.state.items_backup[this.state.index_item].mass} 
+                        gender={this.state.items_backup[this.state.index_item].gender}
+                        birth_year={this.state.items_backup[this.state.index_item].birth_year}
+                    />
+                    <img id="right-arrow" className="Carousel-arrow" src={right_arrow} title="Right arrow" data-next={right_index} onClick={this.onClickArrow}/>
+                </div>
+            );
+            
+        } else {
+            return (
+                <span>Loading...</span>
+            );
+        }
+        
+    }
+
+    onClickArrow(_ev){
+        var data_next = parseInt(_ev.target.getAttribute('data-next')),
+            left_arrow = document.getElementById('left-arrow'),
+            right_arrow = document.getElementById('right-arrow'),
+            left_index = data_next - 1,
+            right_index = data_next + 1;
+                
+            if(left_index < 0){
+                left_index = this.state.items_backup.length - 1;
+            }
+            if(right_index > (this.state.items_backup.length - 1)){
+                right_index = 0;
+            }
+            left_arrow.setAttribute('data-next',left_index);
+            right_arrow.setAttribute('data-next',right_index);
+
+        this.setState({index_item: data_next});
+
+    }
+
 }
 
 export default Carousel;
--- src/js/CarouselItem.jsx
+++ src/js/CarouselItem.jsx
@@ -3,8 +3,12 @@ import '../css/CarouselItem.css';
 class CarouselItem extends Component {
     render() {
         return (
-            <div className="CarouselItem">
-                <span>Prueba de nombre largo</span>
+            <div className="CarouselItem" data-index={this.props.index}>
+                <h4 className="CarouselItem-title">{this.props.name}</h4>
+                <span className="CarouselItem-info"><b>Genre: </b>{this.props.gender}</span>
+                <span className="CarouselItem-info"><b>Height: </b>{this.props.height}</span>
+                <span className="CarouselItem-info"><b>Mass: </b>{this.props.mass}</span>
+                <span className="CarouselItem-info"><b>Birth Year: </b>{this.props.birth_year}</span>
             </div>     
         );
     }
